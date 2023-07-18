15450970aa92aeefb2238ed5aeeac5f749bcbc91
peyman-borhani <peyman.borhani@gmail.com>
2023-07-18 17:49:18 +0430

    typo and code correction

--- src/lib/Images.js
+++ src/lib/Images.js
@@ -1,17 +1,20 @@
 
-export const off = 'https://images.unsplash.com/photo-1557672172-298e090bd0f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
+export const off = `https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/${Math.floor(Math.random()* 25 + 1)}.jpeg`;
+console.log(off)
+//'https://images.unsplash.com/photo-1557672172-298e090bd0f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
 
-defer export let pics = Array(8).fill(fillup);
+export let pics = Array(8).fill(fillup);
 
 function  fillup() {  	//"https://picsum.photos/seed/10/200/300?random=1"
 	
-					for(i=0;  i<9;  i++) {					
-							pics[i]= {  id: i+1,
-												  on: false,
-												  alt: 'image '+ (i+1),
-												  src: "https://placem.at/animals?w=300&h=300&random"+(i+2);
-							} console.log(pics[i])
-					}								 
-	};
+	for(let i=0;  i<9;  i++) {					
+			pics[i]= {  id: i+1,
+						on: false,
+						alt: 'image '+ (i+1),
+						src: "https://placem.at/animals?w=300&h=300&random"+(i+2)
+			}
+			console.log(pics[i])
+	}								 
+};
 
 // export const imgs = [ {...}, {...}, .... ]
